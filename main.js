18a1f34cd21966be140f659952a900b0f2702502
larissaCharaoAnaC <127850235+larissaCharaoAnaC@users.noreply.github.com>
2023-09-20 09:37:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,3 +35,14 @@ function tocaSomTom(){
     document.querySelector('.tecla_Tom').onclick = tocaSomTom;} 
 
     document.querySelectorAll('.tecla');
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+    listaDeTeclas[0].onclick = tocaSomPom;
+    listaDeTeclas[0].onclick = tocaSomClap;
+    listaDeTeclas[0].onclick = tocaSomTim;
+    listaDeTeclas[0].onclick = tocaSomPuff;
+    listaDeTeclas[0].onclick = tocaSomSplash;
+    listaDeTeclas[0].onclick = tocaSomToim;
+    listaDeTeclas[0].onclick = tocaSomPsh;
+    listaDeTeclas[0].onclick = tocaSomTic;
+    listaDeTeclas[0].onclick = tocaSomTom;
